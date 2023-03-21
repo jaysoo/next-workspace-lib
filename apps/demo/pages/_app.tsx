0265ce2fde048f2e39cad5a1a370cab737a04818
Jack Hsu <jack.hsu@gmail.com>
2023-03-21 08:46:57 -0400

    add libs

--- apps/demo/pages/_app.tsx
+++ apps/demo/pages/_app.tsx
@@ -1,15 +1,19 @@
 import { AppProps } from 'next/app';
+import { Ui } from '@next-repo/ui'
+import { mylibWp } from '@next-repo/mylib-wp';
 import Head from 'next/head';
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  console.log(mylibWp());
   return (
     <>
       <Head>
         <title>Welcome to demo!</title>
       </Head>
       <main className="app">
-        <Component {...pageProps}/>
+        <Ui />
+        <Component {...pageProps} />
       </main>
     </>
   );
